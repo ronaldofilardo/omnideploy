73bd57a9d8ac08d398762dd693fc0a3aef330e6f
ronaldofilardo <ronaldofilardo@gmail.com>
2025-10-26 23:30:48 -0300

    Corrigir migração para remover apenas PROCEDURE, MEDICATION e OTHER do enum EventType

--- web/prisma/migrations/20251027012122_rename_event_types/migration.sql
+++ web/prisma/migrations/20251027012122_rename_event_types/migration.sql
@@ -1,14 +1,20 @@
 /*
-  Warnings:
+Warnings:
 
-  - The values [CONSULTATION,EXAM] on the enum `EventType` will be removed. If these variants are still used in the database, this will fail.
+- The values [PROCEDURE,MEDICATION,OTHER] on the enum `EventType` will be removed. If these variants are still used in the database, this will fail.
 
 */
 -- AlterEnum
 BEGIN;
-CREATE TYPE "EventType_new" AS ENUM ('CONSULTA', 'EXAME', 'PROCEDURE', 'MEDICATION', 'OTHER');
+
+CREATE TYPE "EventType_new" AS ENUM ('CONSULTA', 'EXAME');
+
 ALTER TABLE "health_events" ALTER COLUMN "type" TYPE "EventType_new" USING ("type"::text::"EventType_new");
+
 ALTER TYPE "EventType" RENAME TO "EventType_old";
+
 ALTER TYPE "EventType_new" RENAME TO "EventType";
+
 DROP TYPE "public"."EventType_old";
+
 COMMIT;
